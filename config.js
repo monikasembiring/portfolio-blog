1307c5c0a1d6dcfffef1f1d8473513c634813b75
monikasembiring <sembiringmonika@gmail.com>
2023-01-05 17:20:27 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  title: 'Portfolio Blog', // Required
+  author: 'Monika Sembiring', // Required
+  description: 'Product manager who is in love with data',
+  primaryColor: '#3498db', // Required
+  showHeaderImage: true,
+  showShareButtons: true,
+  postsPerPage: 5, // Required
+  social: {
+    website: 'https://monikasembiring.github.io/',
+    github: 'https://github.com/monikasembiring',
+    twitter: 'https://twitter.com/monikasembiring',
+    linkedin: 'https://www.linkedin.com/in/monikasembiring/',
+  },
+};
